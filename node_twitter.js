cf752ff3a1edd4866ac652a3711ed35cec389ba5
Fabrizio Codello <fabryz@gmail.com>
2012-01-17 12:52:49 +0100

    added Express 2.5.6

--- node_twitter.js
+++ node_twitter.js
@@ -1,7 +1,42 @@
-var util = require('util'),
+/**
+ * Module dependencies.
+ */
+
+var express = require('express'),
+    util = require('util'),
 	spawn = require('child_process').spawn;
-    
-console.log('* Twitter Streaming app has started.');
+
+var app = module.exports = express.createServer();
+
+// Configuration
+
+app.configure(function(){
+    app.set('views', __dirname + '/views');
+    app.set('view engine', 'jade');
+    app.use(express.bodyParser());
+    app.use(express.methodOverride());
+    app.use(app.router);
+    app.use(express.static(__dirname + '/public'))
+    app.use(express.logger(':remote-addr - :method :url HTTP/:http-version :status :res[content-length] - :response-time ms'));
+    app.use(express.favicon());
+});
+
+app.configure('development', function(){
+    app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
+});
+
+app.configure('production', function(){
+    app.use(express.errorHandler()); 
+});
+
+// Routes
+
+app.get('/', function(req, res){
+  res.render('index', { title: 'Twitter Streaming' })
+});
+
+app.listen(8080);
+console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
     
 var tail_child = spawn('sh', ['stream.sh'], { 'cwd' : __dirname });
 

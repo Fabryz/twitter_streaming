0106cffe9805e02036182e44f4ae530deabbda57
Fabrizio Codello <fabryz@gmail.com>
2012-01-18 00:34:28 +0100

    added decent date format

--- public/js/streaming.js
+++ public/js/streaming.js
@@ -8,6 +8,22 @@ $(document).ready(function() {
 		tweets = $("#tweets ul");
 		
 	status.html("Connecting.");
+
+	Date.prototype.format = function (fmt) {
+		var date = this;
+
+		return fmt.replace(/\{([^}:]+)(?::(\d+))?\}/g, function (s, comp, pad) {
+	        var fn = date["get" + comp];
+
+	        if (fn) {
+	            var v = (fn.call(date) + (/Month$/.test(comp) ? 1 : 0)).toString();
+	
+	            return pad && (pad = pad - v.length) ? new Array(pad + 1).join("0") + v : v;
+	        } else {
+	            return s;
+	        }
+		});
+	};
 	
 	/* 
 	* Socket stuff	
@@ -30,7 +46,9 @@ $(document).ready(function() {
 	});
 
 	socket.on('tweet', function(data) {	
-		tweets.append('<li>'+ data.tweet.created_at +' '+ data.tweet.user.screen_name +': '+ data.tweet.text +'</li>');
+		var date = new Date(data.tweet.created_at);
+
+		tweets.append('<li>'+ date.format("{FullYear}/{Month:2}/{Date:2} {Hours:2}:{Minutes:2}:{Seconds:2}") +' '+ data.tweet.user.screen_name +': '+ data.tweet.text +'</li>');
 		$('#tweets').prop('scrollTop', $('#tweets').prop('scrollHeight'));
 	});
 });
